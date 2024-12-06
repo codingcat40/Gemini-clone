b7cea46b626ad14af6bd65eb0222d9c9c30a5481
naman <naman@fieldproxy.com>
2024-12-06 22:15:48 +0530

    Sidebar collapse

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,84 @@
+import { Button } from "@mui/material";
 import { assets } from "../../assets/assets";
-import AddIcon from '@mui/icons-material/Add';
+import { useState } from "react";
+
 const Sidebar = () => {
+  const [toggleSidebar, setSidebarToggle] = useState(false);
+
   return (
-    <div className="bg-slate-100 min-w-80 max-w-52 min-h-full space-y-8">
-      <div>
-        <img src={assets.menu_icon} alt="" />
-      </div>
+    <div
+      className={`bg-slate-100 h-screen flex flex-col justify-between p-4 transition-all duration-300 ${
+        toggleSidebar
+          ? "min-w-[20rem] max-w-[20rem]"
+          : "min-w-[7rem] max-w-[7rem]"
+      }`}
+    >
+      {" "}
+      {/* Top Section */}
+      <div className="flex flex-col gap-4">
+        <Button
+          className="flex flex-row items-start max-w-12"
+          onClick={() => setSidebarToggle(!toggleSidebar)}
+        >
+          <img src={assets.menu_icon} className="w-[2rem]" alt="Menu" />
+        </Button>
 
-      <div>
-        <div className="flex flex-row">
-          {/* <img src={assets.plus_icon} className="size-8" alt="" /> */}
-          <AddIcon className="size-16"/>
-          <p>New chat</p>
+        <div className="flex flex-col gap-2">
+          <Button
+            className="flex flex-row gap-2 h-12 items-start rounded-full min-w-12 max-w-28"
+            size={"small"}
+          >
+            <img src={assets.plus_icon} className="w-[1rem]" alt="Plus" />
+            {toggleSidebar && (
+              <p className="text-gray-500 text-sm font-medium">New chat</p>
+            )}
+          </Button>
+
+          {toggleSidebar && (
+            <div>
+            <p className="text-gray-500 text-sm font-medium mt-4">Recent</p>
+            <Button className="flex flex-row items-start w-[12rem]">
+                <img src={assets.message_icon} alt="" className="w-8 h-8" />
+                <p className="text-black text-xs font-light w-[7rem]">what is react ?</p>
+            </Button>
+            </div>
+          )}
         </div>
       </div>
+      {/* Bottom Section */}
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <Button className="flex flex-row gap-2 items-center h-8">
+            <img src={assets.question_icon} className="w-[1rem]" alt="Help" />
+            {toggleSidebar && (
+              <p className="text-black text-sm font-medium">Help</p>
+            )}
+            <span className="w-2 h-2 bg-red-500 rounded-full ml-auto"></span>
+          </Button>
 
-      <div>
-        <div className="flex flex-row">
-          <img src={assets.question_icon} alt="" />
-          <p>Help</p>
-        </div>
-        <div className="flex flex-row">
-          <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
-        </div>
-        <div className="flex flex-row">
-          <img src={assets.setting_icon} alt="" />
-          <p>Settings</p>
+          <Button className="flex flex-row gap-2 items-center h-8">
+            <img
+              src={assets.history_icon}
+              className="w-[1rem]"
+              alt="Activity"
+            />
+            {toggleSidebar && (
+              <p className="text-black text-sm font-medium">Activity</p>
+            )}
+            <span className="ml-auto"></span>
+          </Button>
+
+          <Button className="flex flex-row gap-2 items-center h-8">
+            <img
+              src={assets.setting_icon}
+              className="w-[1rem]"
+              alt="Settings"
+            />
+            {toggleSidebar && (
+              <p className="text-black text-sm font-medium">Settings</p>
+            )}
+            <span className="w-2 h-2 bg-red-500 rounded-full ml-auto"></span>
+          </Button>
         </div>
       </div>
     </div>
