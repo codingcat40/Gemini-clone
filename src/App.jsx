92d46af33a85cc28c1ca62848e543347a14562e1
naman <naman@fieldproxy.com>
2024-12-23 00:27:53 +0530

    Adding the Main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar' 
+import Main from './components/Main/Main'
 
 const App = () => {
   return (
-    <>
+    <div className='flex flex-row'>
       <Sidebar />
-    </>
+      <Main />
+    </div>
   )
 }
 
